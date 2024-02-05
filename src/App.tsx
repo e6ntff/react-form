6cb3cf71c0cc0a2b707b9d40502af9e703065c19
e6ntff <pashatar2@gmail.com>
2024-02-05 20:38:52 +0300

    added app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from './components/Form';
+import Result from './components/Result';
+
+interface Element {
+	value: string;
+	valid: boolean;
+}
+
+export interface Data {
+	nickname: Element;
+	email: Element;
+	password: Element;
+	passwordRepeat: Element;
+	text: Element;
+}
 
 const App: React.FC = () => {
-	return <></>;
+	const [isEnded, setIsEnded] = useState<boolean>(false);
+	const [data, setData] = useState<Data>({
+		nickname: {
+			value: '',
+			valid: false,
+		},
+		email: {
+			value: '',
+			valid: false,
+		},
+		password: {
+			value: '',
+			valid: false,
+		},
+		passwordRepeat: {
+			value: '',
+			valid: false,
+		},
+		text: {
+			value: '',
+			valid: true,
+		},
+	});
+
+	return (
+		<>
+			{!isEnded ? (
+				<Form
+					data={data}
+					setData={setData}
+					setIsEnded={setIsEnded}
+				/>
+			) : (
+				<Result data={data} />
+			)}
+		</>
+	);
 };
 
 export default App;
